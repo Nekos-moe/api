a848c9f0882b9105f0f625eee08cabe819eeed84
MinekPo1 <68391527+MinekPo1@users.noreply.github.com>
2021-04-12 15:56:11 +0200

    Added funct for specifing a tag for random/image. UNTESTED but should be fine I hope. I just copied code from one file to another.

--- api/v1/routes/ImageRandomGET.js
+++ api/v1/routes/ImageRandomGET.js
@@ -17,8 +17,39 @@ class ImageRandomGET {
 
 	async run(req, res) {
 		let agg = this.database.Image.aggregate().cursor({ });
+
+		options = {}
+
 		if (req.query.nsfw !== undefined)
-			agg.match({ nsfw: req.query.nsfw == 'true' });
+			options.nsfw = ['true','True',true].includes(req.query.nsfw);
+
+		/* code copied from ImageSearchPOST.js
+		 * written by brussell98
+		*/
+		if (Array.isArray(req.body.tags))
+			req.body.tags = req.body.tags.join(', ');
+
+		if (req.body.tags !== undefined && req.body.tags.trim() !== '') {
+			/* What we are doing here is bypassing a mongodb $text restriction.
+			 * If you only include negate expressions then nothing will match so
+			 * we turn it into a $not regex that matches negated tags and returns
+			 * the rest. This is needed for tag blacklists.
+			*/
+			if (req.body.tags.split(/-"?[^",]+"?(?:, *)?/).join('').trim() === '') {
+				options.tags = {
+					$nin: req.body.tags.match(/(^|, *)-[^,]+/g).map(e => e.replace(/,? *-|"/g, ''))
+				};
+			} else {
+				options.$text = { $search: req.body.tags };
+
+				if (req.body.sort && req.body.sort === 'relevance') {
+					projection.score = { $meta: 'textScore' };
+					sort.score = { $meta: 'textScore' };
+				}
+			}
+		}
+
+		agg.match(options)
 
 		if (req.query.count !== undefined && req.query.count != '0' && /^\d{1,3}$/.test(req.query.count)) {
 			let count = parseInt(req.query.count, 10);
